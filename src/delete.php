c4e09daa2946fe8812957007d49907a33891c9ab
jji-min <6wnla9@naver.com>
2024-10-14 17:57:54 +0900

    241014 1차 프로젝트 삭제 페이지

---
+++ src/delete.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/delete.css">
+    <title>삭제 페이지</title>
+</head>
+<body>
+    <main>
+        <div class="main_container">
+            <div class="main_container_box">
+                <div class="main_box_left"></div>
+                <div class="detail_container_box">
+                    <div class="detail_top_tape"></div>
+                    <div class="detail_back_green">
+                        <img src="./img/theme/pixel_sticker1.png" alt="" class="detail_sticker1">
+                        <img src="./img/theme/pixel_sticker2.png" alt="" class="detail_sticker2">
+                        <img src="./img/theme/pixel_sticker3.png" alt="" class="detail_sticker3">
+                        <div class="detail_back_white">
+                            <div class="delete_comment">
+                                <p>삭제하시면 영구적으로 사라집니다.</p>
+                                <p>정말로 삭제 하시겠습니까?</p>
+                            </div>
+                            <div class="detail_content_date">2024-10-15</div>
+                            <div>
+                                <div class="detail_content_todo">
+                                    <div>
+                                        <a href="#"><img src="./img/checkbox.png" alt="" class="detail_content_checkbox"></a>
+                                    </div>
+                                    <div style="padding-top: 5px;">빨래하기</div>
+                                </div>
+                                <div class="detail_content_timestamp">2024.01.01 00:00:00</div>
+                            </div>
+                            <div>
+                                <a href="./list.php"><button type="button" class="btn_small">삭제</button></a>
+                                <a href="./detail.php"><button type="button" class="btn_small">취소</button></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail_bottom_tape"></div>
+                </div>
+                <div class="main_box_right"></div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
