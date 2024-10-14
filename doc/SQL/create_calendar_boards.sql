4b339812d66c358680811dbb2c27c2d18c53285c
woominju0208 <alswn5033@gmail.com>
2024-10-14 13:21:08 +0900

    241014 1차프로젝트 SQL수정

--- doc/SQL/create_calendar_boards.sql
+++ doc/SQL/create_calendar_boards.sql
@@ -5,8 +5,16 @@ CREATE TABLE calendar_boards (
 	,year 				INT(5) 					NOT NULL
 	,month 				INT(5) 					NOT NULL
 	,day 					INT(5) 					NOT NULL
+	,weather				VARCHAR(1000)
+	,emotion				VARCHAR(1000)
+	,theme				CHAR(1)					DEFAULT '3'		COMMENT '0이면 동물, 1이면 식물, 2면 픽셀, 3은 없음'
+	,created_at 	TIMESTAMP				NOT NULL	DEFAULT CURRENT_TIMESTAMP()
+	,updated_at 	TIMESTAMP				NOT NULL	DEFAULT CURRENT_TIMESTAMP()
+	,deleted_at 	TIMESTAMP
+);
+
+
 	,memo_content		VARCHAR(500)
 	,memo_created_at 	TIMESTAMP 				DEFAULT CURRENT_TIMESTAMP()
 	,memo_updated_at 	TIMESTAMP 				DEFAULT CURRENT_TIMESTAMP()
 	,memo_deleted_at 	TIMESTAMP
-);
