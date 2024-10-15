36bc9b2edd5ec825708939c987eb42ea81b29f9a
jhg5860 <jaehoon6521@naver.com>
2024-10-15 16:24:08 +0900

    241015 1차 프로젝트 메인페이지 달력 간격 수정

--- src/main.php
+++ src/main.php
@@ -4,11 +4,11 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/config.php");
 require_once(MY_PATH_DB_LIB);
 
 // GET으로 넘겨 받은 year값이 있다면 넘겨 받은걸 year변수에 적용하고 없다면 현재 년도
-$year = isset($_GET['year']) ? $_GET['year'] : date('Y');
+$year = !empty($_GET['year']) ? $_GET['year'] : date('Y');
 // GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
-$month = isset($_GET['month']) ? $_GET['month'] : date('m');
+$month = !empty($_GET['month']) ? $_GET['month'] : date('m');
 // GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
-$day = isset($_GET['day']) ? $_GET['day'] : date('d');
+$day = !empty($_GET['day']) ? $_GET['day'] : date('d');
 
 $date = "$year-$month-01"; // 현재 날짜
 $time = strtotime($date); // 현재 날짜의 타임스탬프
