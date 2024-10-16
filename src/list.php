e4eab35cc61ecd8d9ecb5ad991e8ca8007575634
jji-min <6wnla9@naver.com>
2024-10-16 17:05:02 +0900

    241016 1차 프로젝트 리스트 페이지 수정

--- src/list.php
+++ src/list.php
@@ -147,7 +147,12 @@
                                     <div class="days"> 
                                         <!-- 1일부터 7일 (한 주) -->
                                         <?php for ($k = 0; $k < 7; $k++) { ?> 
-                                            <a class="list_a_color day" href="/list.php?year=<?php echo $year ?>&month=<?php echo $month ?>&day=<?php echo $n ?>">
+                                            <a class="list_a_color day"
+                                                <?php if($k >= $start_week) {
+                                                    if($n <= $total_day) { ?>
+                                                    href="/list.php?year=<?php echo $year ?>&month=<?php echo $month ?>&day=<?php echo $n ?>"
+                                                    <?php } 
+                                                } ?>>
                                                 <!-- 시작 요일부터 마지막 날짜까지만 날짜를 보여주도록 -->
                                                 <?php if ( ($n > 1 || $k >= $start_week) && ($total_day >= $n) ) { ?>
                                                     <!-- 현재 날짜를 보여주고 1씩 더해줌 -->
